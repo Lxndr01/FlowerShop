b0a255c3487d4ff3c29ffcc1e362c9f84a5c613f
Hajtó Kornél <kornelhajto2004@gmail.com>
2023-04-06 22:31:49 +0200

    Create ViewController.cs
    Added controller for views.

---
+++ Controllers/ViewController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ViragokAPI.DataBase;
+using ViragokAPI.Models;
+
+namespace ViragokAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViewController : ControllerBase
+    {
+        [HttpGet("vviragok")]
+        public async Task<IActionResult> GetVViragok()
+        {
+            APIResponse response = new APIResponse();
+            try
+            {
+                using (SQL sql = new SQL())
+                {
+                    var vviragok = await sql.v_viragok.OrderBy(a => a.id).ToListAsync();
+                    response.StatusCode = 200;
+                    response.Message = "Sikeres lekérdezés!";
+                    response.Data = vviragok;
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 500;
+                response.Message = ex.Message;
+                response.Data = ex;
+
+            }
+            return BadRequest(response);
+        }
+        [HttpGet("vertekesites")]
+        public async Task<IActionResult> GetVErtekesitesek()
+        {
+            APIResponse response = new APIResponse();
+            try
+            {
+                using (SQL sql = new SQL())
+                {
+                    var vviragok = await sql.v_ertekesitesek.OrderBy(a => a.id).ToListAsync();
+                    response.StatusCode = 200;
+                    response.Message = "Sikeres lekérdezés!";
+                    response.Data = vviragok;
+                }
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.StatusCode = 500;
+                response.Message = ex.Message;
+                response.Data = ex;
+
+            }
+            return BadRequest(response);
+        }
+    }
+}
